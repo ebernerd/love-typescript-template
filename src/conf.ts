e62fe4772273ba3797f6fd3153308f7a1e14b9d2
Eric Bernard <eric99b@gmail.com>
2020-05-27 01:22:32 -0400

    Remove built-in prettier config, flesh out default main.ts with more love callbacks

--- src/conf.ts
+++ src/conf.ts
@@ -3,9 +3,9 @@
 // Use `yarn add <username>/<github repo name>` to install a Lua module.
 //
 // package.path may have to be modified to link to certain modules correctly.
-package.path += ";node_modules/?/init.lua";
-package.path += ";node_modules/?/?.lua";
+package.path += ";node_modules/?/init.lua"
+package.path += ";node_modules/?/?.lua"
 
 love.conf = t => {
-    t.window.title = "TypeScript Project";
-};
+	t.window.title = "TypeScript Project"
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-love.load = () => {
-    const [content] = love.filesystem.read("res/index.txt");
-    print(content);
-};
+love.load = () => {}
 
-love.draw = () => {
-    love.graphics.print("Hello World!", 400, 300);
-};
+love.update = (dt: number) => {}
+
+love.draw = () => {}
+
+love.mousepressed = (x, y, button, isTouch) => {}
+
+love.mousereleased = (x, y, button, isTouch) => {}
+
+love.keypressed = (key, scancode, isRepeat) => {}
+
+love.keyreleased = (key, scancode) => {
+	// Close on escape
+	if (key == "escape") {
+		love.event.quit()
+	}
+}
+
+love.wheelmoved = (x, y) => {}
